049ac5063052841d1a346c069e017d421f27be11
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-06 01:24:01 -0600

    functions and inner functions lab continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(value = "roller-skate")
+{
+    return "This Saturday, I want to " + value + "!";
+}
+
+function mondayWork(value = "go to the office")
+{
+    return "This Monday, I will " + value + ".";
+}
+
+function wrapAdjective(wrapchar = "*")
+{
+    const mystr = "You are ";
+    return inner = function (value = "special") {
+        return mystr + wrapchar + value + wrapchar + "!";
+    };
+}
